ef6ed48f7b369922cc9a0603ff5af0b7859a63b4
amit-tray <amit.kumar@tray.com>
2024-05-07 18:05:10 +0530

    graphql demo

---
+++ src/test/java/com/restart/graphqldemo/GraphqlDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restart.graphqldemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphqlDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
